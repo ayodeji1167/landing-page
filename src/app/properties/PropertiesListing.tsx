01115a39e76104849817051b3dd41a489e75802a
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-02-24 10:30:16 +0100

    phonecard done

--- src/app/properties/PropertiesListing.tsx
+++ src/app/properties/PropertiesListing.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
-
+import Box from '@/components/Ui/chakra/Box';
+import Grid from '@/components/Ui/chakra/Grid';
+import GridItem from '@/components/Ui/chakra/GridItem';
+import SizeWrapper from '@/components/SizeWrapper/SizeWrapper';
+import { properties } from '@/data/propertiesLists';
+import PropertyCard from './components/propertieslisting/PropertyCard';
 export default function PropertiesListing() {
-  return <div>PropertiesListing</div>;
+  return (
+    <Box mt={'3.7rem'}>
+      <SizeWrapper>
+        <Grid
+          templateColumns={{
+            base: 'repeat(2, 1fr)',
+            md: 'repeat(3, 1fr)',
+            lg: 'repeat(3, 1fr)',
+            xl: 'repeat(3, 1fr)',
+          }}
+          gap={'1.5rem'}
+          rowGap={'3rem'}
+        >
+          {properties.map((item, index) => (
+            <GridItem key={index}>
+              <PropertyCard {...item} />
+            </GridItem>
+          ))}
+        </Grid>
+      </SizeWrapper>
+    </Box>
+  );
 }
--- src/app/properties/components/propertieslisting/PropertyCard.tsx
+++ src/app/properties/components/propertieslisting/PropertyCard.tsx
@@ -1,4 +1,76 @@
+import { IProperty } from '@/types/property';
 import React from 'react';
-export default function PropertyCard() {
-  return <div>PropertyCard</div>;
+import Box from '@/components/Ui/chakra/Box';
+import Flex from '@/components/Ui/chakra/Flex';
+import Image from '@/components/Ui/chakra/Image';
+import Text from '@/components/Ui/chakra/Text';
+import locationImg from '@/assets/svg/location.svg';
+import bedsvg from '@/assets/svg/bed.svg';
+import bathsvg from '@/assets/svg/bath.svg';
+import sqfeet from '@/assets/svg/sqfeet.svg';
+
+function Item({ icon, value }: { icon: any; value: string }) {
+  return (
+    <Flex color={'#424955'} gap={'.3rem'} alignItems={'center'}>
+      <Box>
+        <Image alt={value} src={icon.src} />
+      </Box>
+      <Text fontSize={'14px'}>{value}</Text>
+    </Flex>
+  );
+}
+export default function PropertyCard(props: IProperty) {
+  const mainImage = props?.images?.mainImage?.src;
+  const { name, price, pricePerSquareFeet, bed, bath, location, squareFeet } =
+    props;
+
+  return (
+    <Box
+      overflow={'hidden'}
+      position={'relative'}
+      boxShadow="0px 5px 10px 0px #A3A3A31A, 0px 19px 19px 0px #A3A3A317, 0px 42px 25px 0px #A3A3A30D, 0px 74px 30px 0px #A3A3A303, 0px 116px 32px 0px #A3A3A300"
+      rounded={'0.6rem'}
+    >
+      <Box h={'15rem'}>
+        <Image
+          src={mainImage}
+          alt={name}
+          w="100%"
+          h="100%"
+          objectFit={'cover'}
+        />
+      </Box>
+      <Box p={'1.4rem'}>
+        <Text fontFamily={'heading'} fontWeight={700} fontSize={'1.2rem'}>
+          {name}
+        </Text>
+        <Flex mb={'1.4rem'} gap={'.5rem'} alignItems={'center'}>
+          <Image alt="location" src={locationImg.src} />
+          <Text fontSize={'14px'} color="#424955">
+            {location}
+          </Text>
+        </Flex>
+
+        <Flex gap={'1.3rem'} mt={'1.2rem'} alignItems={'center'}>
+          <Item icon={bedsvg} value={`${bed} Bed`} />
+          <Item icon={bathsvg} value={`${bath} Bath`} />
+          <Item icon={sqfeet} value={`${squareFeet} Sqft`} />
+        </Flex>
+
+        <Flex
+          mt={'2rem'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+        >
+          {pricePerSquareFeet && (
+            <Text
+              color={'#424955'}
+              fontSize={'14px'}
+            >{`AED ${pricePerSquareFeet}Per Sqft`}</Text>
+          )}
+          <Text color={'#1B2432'} fontWeight={700}>{`Price AED ${price}`}</Text>
+        </Flex>
+      </Box>
+    </Box>
+  );
 }
