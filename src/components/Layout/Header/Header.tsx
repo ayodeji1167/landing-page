4f0990969953d1df75c9046fe1c79be2ae7ac663
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-02-29 17:36:03 +0100

    added hambuger

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Logo } from '@/components/Logo/Logo';
 import Link from 'next/link';
 import NavItem from './NavItem';
 import SizeWrapper from '@/components/SizeWrapper/SizeWrapper';
-
+import { IoIosMenu } from 'react-icons/io';
 export default function Header() {
   return (
     <header>
@@ -15,15 +15,24 @@ export default function Header() {
           <Box w={'4rem'} cursor={'pointer'}>
             <Logo to="/" />
           </Box>
-          <Flex alignItems={'center'} gap={'2.3rem'}>
+          <Flex
+            display={{ base: 'none', md: 'block' }}
+            alignItems={'center'}
+            gap={'2.3rem'}
+          >
             <NavItem link="/" name="Home" />
             <NavItem link="/properties" name="Properties" />
             <NavItem link="/about" name="About" />
             <NavItem link="/contact" name="Contact" />
           </Flex>
-          <Link href={'/contact'}>
-            <Button>Inquire</Button>
-          </Link>
+          <Box display={{ base: 'none', md: 'block' }}>
+            <Link href={'/contact'}>
+              <Button>Inquire</Button>
+            </Link>
+          </Box>
+          <Box display={{ base: 'block', md: 'none' }}>
+            <IoIosMenu cursor={'pointer'} size={'2.2rem'} />
+          </Box>
         </Flex>
       </SizeWrapper>
     </header>
