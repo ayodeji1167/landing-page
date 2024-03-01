e638af5bb230b0fc2f212129521d92ce5d327898
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-03-01 06:11:28 +0100

    get to know bats responsiveness done

--- src/components/HomePage/GetToKnow/GetToKnow.tsx
+++ src/components/HomePage/GetToKnow/GetToKnow.tsx
@@ -10,16 +10,24 @@ import { Button } from '@chakra-ui/react';
 import { MdOutlineArrowForwardIos } from 'react-icons/md';
 export default function GetToKnow() {
   return (
-    <Box py={'6rem'}>
+    <Box py={{ base: '4rem', md: '6rem' }}>
       <SizeWrapper>
-        <Flex gap={'2.5rem'} alignItems={'center'}>
+        <Flex
+          gap={{ base: '1.2rem', md: '2.5rem' }}
+          direction={{ base: 'column', md: 'row' }}
+          alignItems={'center'}
+        >
           <Box flex={1} h={'25rem'}>
             <Image w={'100%'} h={'100%'} src={img.src} alt="about-us" />
           </Box>
-          <Stack alignItems={'flex-start'} spacing={'1rem'} flex={1}>
+          <Stack
+            alignItems={'flex-start'}
+            spacing={{ base: '.7rem', md: '1rem' }}
+            flex={1}
+          >
             <Text
               color={'gray.700'}
-              fontSize={'2rem'}
+              fontSize={{ base: '1rem', md: '2rem' }}
               fontFamily={'heading'}
               fontWeight={700}
               as={'h3'}
@@ -29,8 +37,8 @@ export default function GetToKnow() {
             </Text>
             <Text
               color={'gray.300'}
-              fontSize={'1rem'}
-              lineHeight={'1.4rem'}
+              fontSize={{ base: '0.8rem', md: '1rem' }}
+              lineHeight={{ base: '1rem', md: '1.4rem' }}
               fontWeight={'400'}
             >
               {` At BATs Properties, we are dedicated to redefining the real estate
@@ -45,6 +53,7 @@ export default function GetToKnow() {
               Dubai.`}
             </Text>
             <Button
+              display={{ base: 'none', md: 'flex' }}
               color={'primary.500'}
               variant={'ghost'}
               _hover={{ bg: 'transparent' }}
@@ -53,6 +62,16 @@ export default function GetToKnow() {
               <Text mr={'.5rem'}> Learn More</Text>
               <MdOutlineArrowForwardIos />
             </Button>
+            <Box
+              color={'primary.500'}
+              display={{ base: 'flex', md: 'none' }}
+              _hover={{ bg: 'transparent' }}
+              fontWeight={'600'}
+              alignItems={'center'}
+            >
+              <Text mr={'.5rem'}> Learn More</Text>
+              <MdOutlineArrowForwardIos size={'.8rem'} />
+            </Box>
           </Stack>
         </Flex>
       </SizeWrapper>
